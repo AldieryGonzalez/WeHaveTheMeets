aabd65932c9f71ecbdb335d4ed3d21ce318ed815
Aldiery <aldieryrgon@gmail.com>
2024-04-06 20:24:51 -0500

    Setting up supabase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Map from "./components/Map";
+import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 function App() {
-    console.log(import.meta.env.VITE_MAPBOX_TOKEN);
     return (
-        <div className='font-bold'>
-            <Map />
-        </div>
+        <AddToCalendarButton
+            name='Title'
+            options={[
+                "Apple",
+                "Google",
+                "Microsoft365",
+                "Outlook.com",
+                "Yahoo",
+            ]}
+            location=''
+            startDate='2024-04-07'
+            endDate='2024-04-07'
+            startTime='10:15'
+            endTime='23:30'
+            timeZone='America/Los_Angeles'
+        ></AddToCalendarButton>
     );
 }
 
