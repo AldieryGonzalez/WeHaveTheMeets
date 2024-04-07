57b9742069d8c99967d71be8459ca856b4e2b610
Yaurie <yaurieh@gmail.com>
2024-04-06 23:23:34 -0500

    find meet set up

--- src/pages/FindMeet.tsx
+++ src/pages/FindMeet.tsx
@@ -1,5 +1,21 @@
+
 const FindMeet = () => {
-    return <div>FindMeet</div>;
+    return (
+        <div className="flex flex-col item-center text-center">
+            <h1 className="text-2xl font-semibold space-y-2 m-10">
+                what's your code?
+            </h1>
+            <br></br>
+            <input className="flex text-center" placeholder="enter it here">
+            </input>
+            <div>
+                <button className="bg-slate-300 rounded px-2 py-1">
+                    Find Meet
+                </button>
+            </div>
+        </div>
+
+    );
 };
 
 export default FindMeet;
